423f9a87b92bdcba0841812ad5559992c7aa2682
tbeaurain <thibaut@thibautbeaurain.home>
2017-04-25 13:35:58 +0200

    ajout de la class connection bdd modif

--- src/ConnectionBDD.java
+++ src/ConnectionBDD.java
@@ -3,12 +3,21 @@ import java.sql.*;
 public class ConnectionBDD {
 
 	public static Connection connection(){
-		String url = "";
+
+		try {
+		    Class.forName( "com.mysql.jdbc.Driver" );
+		} catch ( ClassNotFoundException e ) {
+			System.out.println(e);
+		}
+
+		String url = "jdbc:mysql://localhost:8889/lisa";
 		String utilisateur = "root";
 		String motDePasse = "root";
 		Connection connexion = null;
+		
 		try {
 		    connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+
 		} catch ( SQLException e ) {
 		} finally {
 		    if ( connexion != null )
@@ -17,7 +26,12 @@ public class ConnectionBDD {
 		        } catch ( SQLException ignore ) {
 		        }
 		}
+		
 		return connexion;
+	}
+
+	public static void main(String[] args){
+		connection();
 	}
 
 }
