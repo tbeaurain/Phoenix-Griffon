62dde699078bdd0a62faa77c098b2871efd38e76
francoisrobard <francois2.robard@orange.fr>
2017-06-14 03:41:22 +0200

    correction erreur menu contact

--- src/Controllers/Forms/FormContact.java
+++ src/Controllers/Forms/FormContact.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.phoenixgriffon.JobIsep.Utilisateur;
 
@@ -24,6 +25,10 @@ public class FormContact extends HttpServlet {
 	
 	public static final String ATT_RECHERCHE = "recherche";
 	public static final String VUE_SUCCES = "/WEB-INF/Commun/Contacts.jsp";
+	public static final String ATT_UTILISATEUR = "utilisateur"; // Nom de l'élément Représentant l'utilisateur disponible dans les JSP
+	public static final String ATT_SESSION_USER = "sessionUtilisateur"; // Identifiant de la variable de Session contenant l'utilisateur courant
+    public static final String ATT_USER_TYPE = "typeUtilisateur"; // Variable qui servira à identifier le type d'utilisateur (élève ou admin) dans la BDD
+   
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -44,6 +49,14 @@ public class FormContact extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Utilisateur user = (Utilisateur)session.getAttribute(ATT_SESSION_USER);
+
+		request.setAttribute(ATT_UTILISATEUR, user);
+		
+		int typeUtilisateur = user.getStatutUtilisateur().getId();
+		request.setAttribute(ATT_USER_TYPE, typeUtilisateur);
+		
 		request.setCharacterEncoding("utf-8");
 		String recherche = request.getParameter("titre");
 		ArrayList<Utilisateur> liste_utilisateurs = null;
